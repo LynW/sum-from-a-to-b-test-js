5dacfb122795be61544fa70fa74911472796a1a7
lynw <carolyn.wang94@gmail.com>
2022-04-10 22:06:31 -0400

    completed

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+
+  //function stops once it hits toN
+  if (fromN === toN) {
+    return fromN;
+  }
+
+  return fromN + sum(fromN + 1, toN);
+
 }
 
 module.exports = sum;
